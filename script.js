7a9ea1609fce32247917dfe2d041076faa62de3b
Marek Szumny <lukmarcus@gmail.com>
2023-11-22 23:56:51 +0100

    testing new version

--- script.js
+++ script.js
@@ -4,3 +4,28 @@ window.onload = function () {
     "*"
   );
 };
+
+document.addEventListener("DOMContentLoaded", () => {
+  const switchLangButton = document.getElementById("switch-lang");
+
+  // Initialize the button text based on the current language
+  const updateButtonText = (lang) => {
+    switchLangButton.textContent = lang === "en" ? "Switch to PL" : "Przełącz na EN";
+  };
+
+  // Set the initial button text and get the current language
+  let currentLang = switchLangButton.getAttribute("data-current-lang");
+  updateButtonText(currentLang);
+
+  // Add click event listener to toggle language and update button text
+  switchLangButton.addEventListener("click", () => {
+    currentLang = currentLang === "en" ? "pl" : "en";
+    switchLangButton.setAttribute("data-current-lang", currentLang);
+    updateButtonText(currentLang);
+
+    // Toggle elements based on the language
+    document.querySelectorAll("[data-lang]").forEach((el) => {
+      el.style.display = el.getAttribute("data-lang") === currentLang ? "" : "none";
+    });
+  });
+});
