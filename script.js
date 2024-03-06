fa7a88aab174bc86cdf497c0835ce9ffff7695df
Marek Szumny <lukmarcus@gmail.com>
2024-03-07 00:03:33 +0100

    Changes in script file and display none removed

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ window.onload = function () {
 document.addEventListener("DOMContentLoaded", () => {
   const switchLangButton = document.getElementById("switch-lang");
 
+  // Function to toggle language elements based on the current language
+  const toggleLanguageElements = (lang) => {
+    document.querySelectorAll("[data-lang]").forEach((el) => {
+      el.hidden = el.getAttribute("data-lang") !== lang;
+    });
+  };
+
   // Initialize the button text based on the current language
   const updateButtonText = (lang) => {
     switchLangButton.textContent = lang === "en" ? "Switch to PL" : "Przełącz na EN";
@@ -16,16 +23,13 @@ document.addEventListener("DOMContentLoaded", () => {
   // Set the initial button text and get the current language
   let currentLang = switchLangButton.getAttribute("data-current-lang");
   updateButtonText(currentLang);
+  toggleLanguageElements(currentLang);
 
   // Add click event listener to toggle language and update button text
   switchLangButton.addEventListener("click", () => {
     currentLang = currentLang === "en" ? "pl" : "en";
     switchLangButton.setAttribute("data-current-lang", currentLang);
     updateButtonText(currentLang);
-
-    // Toggle elements based on the language
-    document.querySelectorAll("[data-lang]").forEach((el) => {
-      el.style.display = el.getAttribute("data-lang") === currentLang ? "" : "none";
-    });
+    toggleLanguageElements(currentLang);
   });
 });
